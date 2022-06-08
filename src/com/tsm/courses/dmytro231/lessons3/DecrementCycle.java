6b46229b92a0c56aa7c0afdb912f6c3baeb0ede6
Ripinsky Dmitro <dmb201652@gmail.com>
2022-06-08 15:50:15 +0300

    lessons 3: Output to terminal 5-1

---
+++ src/com/tsm/courses/dmytro231/lessons3/DecrementCycle.java
@@ -0,0 +1,8 @@
+package com.tsm.courses.dmytro231.lessons3;
+public class DecrementCycle {
+    public static void main(String args[]) {
+        for (int i = 5; i >= 1; i--) {
+            System.out.print(i + " ");
+        }
+    }
+}
