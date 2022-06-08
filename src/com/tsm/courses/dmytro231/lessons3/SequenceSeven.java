98948d9c5cdaf4be0f4e208d727b5820cbc76304
Ripinsky Dmitro <dmb201652@gmail.com>
2022-06-08 15:52:34 +0300

    lessons 3: Output to terminal 7-98

---
+++ src/com/tsm/courses/dmytro231/lessons3/SequenceSeven.java
@@ -0,0 +1,10 @@
+package com.tsm.courses.dmytro231.lessons3;
+public class SequenceSeven {
+    public static void main(String args[]) {
+        int i = 0;
+        while (i < 98) {
+            i += 7;
+            System.out.print(i + " ");
+        }
+    }
+}
