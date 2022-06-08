d985121213e070bb786739103efeea9e0c3317fe
Ripinsky Dmitro <dmb201652@gmail.com>
2022-06-08 15:38:30 +0300

    lessons 3: Output to terminal squared numbers

---
+++ src/com/tsm/courses/dmytro231/lessons3/SquareNumbers.java
@@ -0,0 +1,8 @@
+package com.tsm.courses.dmytro231.lessons3;
+public class SquareNumbers {
+    public static void main(String args[]) {
+        for (int i = 10; i <= 20; i++) {
+            System.out.print( Math. round (Math.pow(i, 2)) +" ");
+        }
+    }
+}
